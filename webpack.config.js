c73b8ba549a95f86f19d75e59d1262c17cebf748
Morgan McDermott <mmcdermo@Morgans-MacBook-Pro.local>
2017-01-27 23:31:08 -0800

    Webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+var babelPolyfill = require('babel-polyfill');
+module.exports = {
+    entry: [
+	'babel-polyfill',
+        "./frontend/js/index.jsx"
+    ],
+    output: {
+        path: __dirname + '/static/bundles/',
+        filename: "bundle.js",
+        publicPath: 'http://localhost:3000/static/bundles/',
+    },
+    module: {
+        loaders: [
+	    { test: /\.(woff)\?v=2\.0\.0$/, loader: 'url-loader',
+	      options: {
+	          limit: 10,
+	          mimetype: 'application/font-woff'
+	      }},
+            { test: /\.jsx$/, loader: 'babel', query: {presets: ['react', 'es2015']}, exclude: /node_modules/ },
+            { test: /\.js$/, loaders: ['babel'], exclude: /node_modules/ },
+            { test: /\.css$/, loader: "style-loader!css-loader"},
+            { test: /\.scss$/, loaders: ["style", "css", "sass"]},
+	    { test: /\.(png|jpg)$/, loader: 'url-loader?limit=1' },
+	    { test: /\.json$/, loader: 'json-loader' }
+        ]
+    },
+    resolve: {
+        modulesDirectories: ['node_modules', 'bower_components'],
+        extensions: ['', '.js', '.jsx']
+    },
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new BundleTracker({filename: './webpack-stats.json'}),
+    ]
+};
